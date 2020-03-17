33c16a297e43aec476c8f4f86b83d945eacf7f63
ana paula <anapaulamak@gmail.com>
2020-03-16 21:56:41 -0300

    Create app

---
+++ giphy-sort/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input></input>
+      <button>Pesquisar</button>
+    </div>
+  );
+}
+
+export default App;
