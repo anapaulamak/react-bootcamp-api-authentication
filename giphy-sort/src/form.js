dc243f086a472e6acb3a89aff2e8af8484121718
ana paula <anapaulamak@gmail.com>
2020-03-19 22:41:35 -0300

    Add iframe to display gif

--- giphy-sort/src/form.js
+++ giphy-sort/src/form.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import './form.styled.css'
 
 export default function Form() {
   const [keyword, setKeyword] = useState()
-  const [giphy, setGiphy] = useState('https://giphy.com/embed/WXB88TeARFVvi')
+  const [giphy, setGiphy] = useState()
 
   const searchGiphy = () => {
     const api_key = 'n3TQWBfGFeDYQWqW0GgUII5S66nldPqC'
@@ -13,9 +14,10 @@ export default function Form() {
     axios
     .get(`http://api.giphy.com/v1/gifs/search?q=${q}&api_key=${api_key}&limit=${limit}`)
     .then((response) => {
-      const url = response.data.data[0].url
+      console.log('response', response)
+      const url = response.data.data[0].embed_url
       setGiphy(url)
-      console.log(giphy)
+      console.log('url', url)
     })
     .catch(function(err) {
       console.error(err);
@@ -24,9 +26,14 @@ export default function Form() {
 
   return (
     <div className="App">
-      <input onChange={e => setKeyword(e.target.value)}/>
-      <button onClick={searchGiphy}>Pesquisar</button>
-      <img src={`"${giphy}"`} alt='my giphy'/>
+      <div className="SearchArea">
+        <input onChange={e => setKeyword(e.target.value)}/>
+        <button onClick={searchGiphy}>Pesquisar</button>
+      </div>
+
+      <div className="GifArea">
+        <iframe src={giphy} title='gif' width="480" height="460" frameBorder="0" />
+      </div>
     </div>
   );
 }
